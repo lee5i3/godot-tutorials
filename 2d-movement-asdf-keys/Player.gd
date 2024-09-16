300505850e330d438bf659cc11b3209b18e0cc9c
Lee Hull <lee5i3@leehull.com>
2024-09-16 18:33:36 -0400

    changes

--- 2d-movement-asdf-keys/Player.gd
+++ 2d-movement-asdf-keys/Player.gd
@@ -1,17 +1,17 @@
 extends CharacterBody2D
 
-@export var speed = 300
+@export var speed:float = 300
 
 # This is a custom function that gets the input from the player
-func get_input():	
+func get_input() -> void:
 	# This is a built-in function that gets the input from the player
-
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+
 	# This is a built-in function that normalizes the input
 	velocity = input_direction * speed
 
 # This is a built-in function that is called every frame
-func _physics_process(_delta):
+func _physics_process(_delta: float) -> void:
 	# This is a custom function that gets the input from the player
 	get_input()
 	# This is a built-in function that moves the player
